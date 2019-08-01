354f965a6b4bb87e793e99d068b27ea9b1cba299
Mark Hegedus <hmark33@gmail.com>
2019-08-01 22:35:09 +0200

    fix(production): fix typo

--- FedEx/src/environments/environment.prod.ts
+++ FedEx/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-	serverURL: 'ec2-54-80-125-138.compute-1.amazonaws.com:3000'
+	serverURL: 'http://ec2-54-80-125-138.compute-1.amazonaws.com:3000'
  };
--- FedEx/src/environments/environment.ts
+++ FedEx/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  serverURL: 'http://localhost:3000'
+  serverURL: 'localhost:3000'
 };
 
 /*
