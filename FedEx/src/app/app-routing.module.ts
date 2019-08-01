3befe75ef47ef34d06c614a5b2260070f4becf8d
Alpar <alpar1899@gmail.com>
2019-08-01 19:10:59 +0200

    feat: homework submit route

--- FedEx/src/app/app-routing.module.ts
+++ FedEx/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeworkCreationComponent } from './homework-creation/homework-creation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'createhomework', component: HomeworkCreationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FedEx/src/app/app.module.ts
+++ FedEx/src/app/app.module.ts
@@ -1,18 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeworkCreationComponent } from './homework-creation/homework-creation.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+ MatButtonModule,
+ MatCardModule,
+ MatCheckboxModule,
+ MatDialogModule,
+ MatFormFieldModule,
+ MatIconModule,
+ MatInputModule,
+ MatMenuModule,
+ MatSidenavModule,
+ MatSelectModule,
+ MatTabsModule,
+ MatTableModule,
+ MatToolbarModule,
+ MatPaginatorModule,
+ MatSnackBarModule,
+ MatExpansionModule,
+ MatBadgeModule,
+ MatDatepickerModule,
+ MatNativeDateModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeworkCreateService } from './services/homework-create.service';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeworkCreationComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+   BrowserAnimationsModule,
+   BrowserModule,
+   AppRoutingModule,
+   FormsModule,
+   ReactiveFormsModule,
+   MatButtonModule,
+   MatCardModule,
+   MatCheckboxModule,
+   MatDialogModule,
+   MatFormFieldModule,
+   MatIconModule,
+   MatInputModule,
+   MatMenuModule,
+   MatSidenavModule,
+   MatSelectModule,
+   MatTabsModule,
+   MatTableModule,
+   MatToolbarModule,
+   MatPaginatorModule,
+   MatSnackBarModule,
+   MatExpansionModule,
+   MatBadgeModule,
+   MatDatepickerModule,
+   MatNativeDateModule,
+   HttpClientModule
   ],
-  providers: [],
+  providers: [HomeworkCreateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
