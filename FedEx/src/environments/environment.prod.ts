f39e86047a73f5ebb4e2fc1bda198ca906572e0a
Mark Hegedus <hmark33@gmail.com>
2019-08-02 11:17:59 +0200

    fix(production): add missing line

--- FedEx/src/environments/environment.prod.ts
+++ FedEx/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-	serverURL: 'http://ec2-54-80-125-138.compute-1.amazonaws.com:3000'
+	serverURL: 'http://ec2-54-80-125-138.compute-1.amazonaws.com:3000',
+	MAX_IMAGE_SIZE: 5000000
 };
