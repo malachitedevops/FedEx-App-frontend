e7d6dc774d164e01fbf6369d86649c9bc05c3b56
tothtomi <tothtomi7@gmail.com>
2019-08-02 03:20:03 +0200

    Auto stash before merge of "homeworkScreen" and "origin/Dev"

--- FedEx/src/app/services/class.service.ts
+++ FedEx/src/app/services/class.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { MatSnackBar } from '@angular/material';
+import { MatSnackBar, MatDialogConfig, MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
 import { DialogService } from './dialog.service';
 import { Subject, Observable } from 'rxjs';
+import { PopupComponent } from '../components/popup/popup.component';
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +17,11 @@ export class ClassService {
   selectedClass = this.selectedClassSubject.asObservable()
 
   constructor(
-    private http: HttpClient, 
-    private router: Router, 
+    private http: HttpClient,
+    private router: Router,
     private dialogService: DialogService,
-    private snackBarNotification: MatSnackBar
+    private snackBarNotification: MatSnackBar,
+    private dialog: MatDialog,
   ) { }
 
   getClasses() {
@@ -33,11 +35,11 @@ export class ClassService {
     return this.classes.asObservable();
   }
 
-  createClass(className:string){
-    this.http.post(`${environment.serverURL}/class`, { className:className }, {
+  createClass(className: string) {
+    this.http.post(`${environment.serverURL}/class`, { className: className }, {
       headers: { 'content-type': 'application/json' }
-    }).subscribe(() => {
-      this.snackBarNotification.open('class added', 'ok', {
+    }).subscribe((data) => {
+      this.snackBarNotification.open(`Your new class code is: ${data['classNumber']}`, 'ok', {
         duration: 5000,
         panelClass: ['snackbar']
       });
