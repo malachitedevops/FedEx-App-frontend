e762bba6ff3296cfe93a86bf4d715a702fde30e2
Andor Auber <andor.auber@gmail.com>
2019-08-02 09:53:12 +0200

    aa

--- FedEx/src/app/app-routing.module.ts
+++ FedEx/src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'homework', component: HomeworkFullComponent},
-  {path: 'createhomework', component: HomeworkCreationComponent}
+  {path: 'createhomework', component: HomeworkCreationComponent},
   {path: 'settings', component: SettingsComponent}
 ];
 
